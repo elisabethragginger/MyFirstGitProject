f265443be328e64b24e10fcf64f04bb885586bc1
elisabethragginger <elisabeth.ragginger@htl-salzburg.ac.at>
2019-01-30 17:42:30 +0100

    changes unity

---
+++ Assets/Scripts/TestObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestObject : MonoBehaviour {
+    
+    string helloworld = "Hello World";
+    
+    public void PrintHelloWorld()
+    {
+        Debug.Log(helloworld);
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
